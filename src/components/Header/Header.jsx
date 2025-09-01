b8414c7a67d306ffda14f8aa8639f3b951913720
Imamul Kadir <imamulkadiroyan@gmail.com>
2025-09-01 08:34:58 +0600

    updated nav active

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,47 @@
 import { Link, NavLink } from "react-router-dom";
-import "./Header.css";
+// import "./Header.css";
 
 const Header = () => {
+  const activeStyle = {
+    backgroundColor: "#323334",
+    color: "#ffffff",
+    borderRadius: "5px",
+    padding: "0.25rem 0.5rem",
+  };
   return (
     <div>
       <nav>
         <span>Router</span>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/users">Users</NavLink>
-        <NavLink to="/posts">Posts</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact Us</NavLink>
+        <NavLink
+          to="/"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/users"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Users
+        </NavLink>
+        <NavLink
+          to="/posts"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Posts
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/contact"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          Contact Us
+        </NavLink>
       </nav>
     </div>
   );
